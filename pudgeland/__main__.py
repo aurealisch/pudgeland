74e0a73dd7ad04b0120d44b542b2bb7a39a82ed5
elaresai <elaresai@gmail.com>
2023-07-12 13:23:28 +0300

    Refactoring main

--- pudgeland/__main__.py
+++ pudgeland/__main__.py
@@ -6,21 +6,17 @@ from .database import databases
 from .environment import environments
 from .model import models
 
-model = models.Model(databases.Database(replit.db))
-
 gateway_bot = hikari.GatewayBot(environments.gateway_bot_token)
 
-client = crescent.Client(gateway_bot, model=model)
+client = crescent.Client(gateway_bot, model=models.Model(databases.Database(replit.db)))
 
-plugins = {
+for plugin in {
     "pudgeland.plugin.action.bite",
     "pudgeland.plugin.action.hug",
     "pudgeland.plugin.action.kiss",
     "pudgeland.plugin.action.lick",
     "pudgeland.plugin.server.query",
-}
-
-for plugin in plugins:
+}:
     client.plugins.load(plugin)
 
 gateway_bot.run()
