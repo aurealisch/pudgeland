48cae6393fb2ec1be3d8c95d1a9e1af033ee08b0
elaresai <elaresai@gmail.com>
2023-07-14 21:39:52 +0300

    Query command component

--- pudgeland/plugin/server/query.py
+++ pudgeland/plugin/server/query.py
@@ -1,11 +1,9 @@
 import crescent
-import hikari
-import mcstatus
 
-from pudgeland.environment import environments
 from pudgeland.locale.plugin import locales
 from pudgeland.plugin import server
 from pudgeland.utility.plugin import plugins
+from pudgeland.component.plugin.server import query
 
 plugin = plugins.Plugin()
 
@@ -36,54 +34,4 @@ plugin = plugins.Plugin()
 class Query:
     # noinspection PyMethodMayBeStatic
     async def callback(self, context: crescent.Context) -> None:
-        query_response = mcstatus.JavaServer(
-            environments.java_server_host,
-            port=environments.java_server_port,
-        ).query()
-
-        players = query_response.players
-        software = query_response.software
-
-        await context.respond(
-            embed=(
-                hikari.Embed(
-                    title="Запрос",
-                    description="""\
-                        Проверяет статус сервера Minecraft Java Edition
-                        с помощью протокола запроса.
-                    """,
-                )
-                .add_field(
-                    "Игроки",
-                    value=f"""\
-                        Онлайн: *{players.online}*
-                        Максимум: *{players.max}*
-                        Имена: ||{
-                            ", ".join(
-                                [
-                                    f"`{name}`"
-
-                                    for name in players.names
-                                ]
-                            )
-                        }||
-                    """,
-                )
-                .add_field(
-                    "Программное обеспечение",
-                    value=f"""\
-                        Марка: *{software.brand}*
-                        Плагины: ||{
-                            ", ".join(
-                                [
-                                    f"`{plugin}`"
-
-                                    for plugin in software.plugins
-                                ]
-                            )
-                        }||
-                        Версия: *{software.version}*
-                    """,
-                )
-            )
-        )
+        await query.Component(plugin).callback(context)
