b2de4de2b12408abb6c5625b2cd2914172c45c62
elaresai <elaresai@gmail.com>
2023-07-14 21:21:07 +0300

    Pat command component

--- pudgeland/plugin/action/pat.py
+++ pudgeland/plugin/action/pat.py
@@ -1,10 +1,10 @@
-import collei
 import crescent
 import hikari
 
 from pudgeland.plugin import action
 from pudgeland.locale.plugin import locales
 from pudgeland.utility.plugin import plugins
+from pudgeland.component.plugin.action import pat
 
 plugin = plugins.Plugin()
 
@@ -40,13 +40,4 @@ class Pat:
 
     # noinspection PyMethodMayBeStatic
     async def callback(self, context: crescent.Context) -> None:
-        await context.respond(
-            embed=(
-                hikari.Embed(
-                    title="Погладить",
-                    description=f"<@{context.user.id}> погладил(а) <@{self.user.id}>",
-                )
-                .set_author(name=context.user.username, icon=context.user.avatar_url)
-                .set_image(collei.Client().sfw.get(collei.SfwCategory.PAT).url)
-            )
-        )
+        await pat.Component().callback(context)
