3ed517c7386c85306b8e7907b45a2da6279e75b2
elaresai <elaresai@gmail.com>
2023-07-14 21:21:29 +0300

    Poke command component

--- pudgeland/plugin/action/poke.py
+++ pudgeland/plugin/action/poke.py
@@ -1,10 +1,10 @@
-import collei
 import crescent
 import hikari
 
 from pudgeland.plugin import action
 from pudgeland.locale.plugin import locales
 from pudgeland.utility.plugin import plugins
+from pudgeland.component.plugin.action import poke
 
 plugin = plugins.Plugin()
 
@@ -40,13 +40,4 @@ class Poke:
 
     # noinspection PyMethodMayBeStatic
     async def callback(self, context: crescent.Context) -> None:
-        await context.respond(
-            embed=(
-                hikari.Embed(
-                    title="Тыкнуть",
-                    description=f"<@{context.user.id}> тыкнул(а) <@{self.user.id}>",
-                )
-                .set_author(name=context.user.username, icon=context.user.avatar_url)
-                .set_image(collei.Client().sfw.get(collei.SfwCategory.POKE).url)
-            )
-        )
+        await poke.Component().callback(context)
