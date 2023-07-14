b44315346990dfabb032914264bbe6e27ba6aa5b
elaresai <elaresai@gmail.com>
2023-07-14 21:39:10 +0300

    Status command component

--- pudgeland/plugin/server/status.py
+++ pudgeland/plugin/server/status.py
@@ -1,11 +1,9 @@
 import crescent
-import hikari
-import mcstatus
 
-from pudgeland.environment import environments
 from pudgeland.locale.plugin import locales
 from pudgeland.plugin import server
 from pudgeland.utility.plugin import plugins
+from pudgeland.component.plugin.server import query
 
 plugin = plugins.Plugin()
 
@@ -36,46 +34,4 @@ plugin = plugins.Plugin()
 class Status:
     # noinspection PyMethodMayBeStatic
     async def callback(self, context: crescent.Context) -> None:
-        java_status_response = mcstatus.JavaServer(
-            environments.java_server_host,
-            port=environments.java_server_port,
-        ).status()
-
-        java_status_players = java_status_response.players
-        java_status_version = java_status_response.version
-
-        await context.respond(
-            embed=(
-                hikari.Embed(
-                    title="Статус",
-                    description="""\
-                        Проверяет статус сервера Minecraft Java Edition
-                        с помощью протокола статуса.
-                    """,
-                )
-                .add_field(
-                    "Игроки",
-                    value=f"""\
-                        Онлайн: *{java_status_players.online}*
-                        Максимум: *{java_status_players.max}*
-                        Образец: {
-                            ", ".join(
-                                [
-                                    f"*{java_status_player.name}"
-                                    f"(||{java_status_player.id}||)"
-
-                                    for java_status_player in java_status_players.sample
-                                ]
-                            )
-                        }
-                    """,
-                )
-                .add_field(
-                    "Версия",
-                    value=f"""\
-                        Имя: *{java_status_version.name}*
-                        Протокол: *{java_status_version.protocol}*
-                    """,
-                )
-            )
-        )
+        await query.Component(plugin).callback(context)
