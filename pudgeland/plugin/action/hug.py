d06879bf32301cee7c7b6c58412d9afee37ab940
elaresai <elaresai@gmail.com>
2023-07-14 21:17:22 +0300

    Hug command component

--- pudgeland/plugin/action/hug.py
+++ pudgeland/plugin/action/hug.py
@@ -1,10 +1,10 @@
-import collei
 import crescent
 import hikari
 
 from pudgeland.plugin import action
 from pudgeland.locale.plugin import locales
 from pudgeland.utility.plugin import plugins
+from pudgeland.component.plugin.action import hug
 
 plugin = plugins.Plugin()
 
@@ -40,13 +40,4 @@ class Hug:
 
     # noinspection PyMethodMayBeStatic
     async def callback(self, context: crescent.Context) -> None:
-        await context.respond(
-            embed=(
-                hikari.Embed(
-                    title="Обнять",
-                    description=f"<@{context.user.id}> обнял(а) <@{self.user.id}>",
-                )
-                .set_author(name=context.user.username, icon=context.user.avatar_url)
-                .set_image(collei.Client().sfw.get(collei.SfwCategory.HUG).url)
-            )
-        )
+        await hug.Component().callback(context)
