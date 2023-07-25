cce6e1bb21eacb01f50a4d396574486fcaa3c0f8
elaresai <elaresai@gmail.com>
2023-07-25 18:43:01 +0300

    Nixpacks plan

--- Procfile
+++
@@ -1,1 +0,0 @@
-start: python -OO -m bot
--- cmd/nixpacks/phases/install.sh
+++
@@ -1,1 +0,0 @@
-python -m venv --copies /opt/venv && . /opt/venv/bin/activate && pip install poetry==$NIXPACKS_POETRY_VERSION && poetry install --no-dev --no-interaction --no-ansi
