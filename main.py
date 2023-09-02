ce1058d246d84d66a9927b64db01d5a1ef50ea77
elaresai <elaresai@gmail.com>
2023-09-02 11:00:16 +0300

    Indentation

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ bot = bots.Bot(
                     economics.Event(
                         "Осень",
                         description=f"""\
-							Я календарь переверну - и снова третье сентября...
+                            Я календарь переверну - и снова третье сентября..
 
-							{emojis.Emoji.berry} Множитель сбора ягод: `2,5x`
-							{emojis.Emoji.fox} Множитель сбора ягод лисами: `1,25x`
-						""",
+                            {emojis.Emoji.berry} Множитель сбора ягод: `2,5x`
+                            {emojis.Emoji.fox} Множитель сбора ягод лисами: `1,25x`
+                        """,
                         buff=economics.Buff(
                             2.5,
                             fox=1.25,
