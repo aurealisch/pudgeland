a3f6cb7c6c9c0a3c5195e91b1df510107bc4ce54
elaresai <elaresai@gmail.com>
2023-09-02 21:27:09 +0300

    Fix

--- main.py
+++ main.py
@@ -11,53 +11,46 @@ dotenv.load_dotenv()
 
 token = os.environ.get("TOKEN")
 
+events = [
+    economics.Event(
+        "Осень",
+        description=f"""\
+            Я календарь переверну - и снова третье сентября..
+
+            {emojis.Emoji.berry} Множитель сбора ягод: `2,5x`
+            {emojis.Emoji.fox} Множитель сбора ягод лисами: `1,25x`
+        """,
+        buff=economics.Buff(
+            2.5,
+            fox=1.25,
+        ),
+    ),
+]
+
+configuration = configurations.Configuration(
+    configurations.Activity("гг сервер умер"),
+    leaders=configurations.Leaders(
+        configurations.Sort("desc"),
+        take=5,
+    ),
+    plugins=configurations.Plugins(
+        configurations.Collect(
+            configurations.Range(75, b=200),
+            foxying=configurations.Range(50, b=135),
+        ),
+        cull=configurations.Cull(2, fraction=0.1),
+        tame=configurations.Tame(2, price=250),
+    ),
+)
+
 bot = bots.Bot(
     token,
     model=models.Model(
         economics.Economics(
-            economics.Configuration(
-                [
-                    economics.Event(
-                        "Осень",
-                        description=f"""\
-                            Я календарь переверну - и снова третье сентября..
-
-                            {emojis.Emoji.berry} Множитель сбора ягод: `2,5x`
-                            {emojis.Emoji.fox} Множитель сбора ягод лисами: `1,25x`
-                        """,
-                        buff=economics.Buff(
-                            2.5,
-                            fox=1.25,
-                        ),
-                    ),
-                ],
-            ),
-            prisma=_prisma.Prisma(),
+            _prisma.Prisma(),
+            events=events,
         ),
-        configuration=configurations.Configuration(
-            configurations.Activity("гг сервер умер"),
-            leaders=configurations.Leaders(
-                configurations.Sort("desc"),
-                take=5,
-            ),
-            plugins=configurations.Plugins(
-                configurations.Collect(
-                    configurations.Range(
-                        75,
-                        b=200,
-                    ),
-                    foxying=configurations.Range(50, b=135.0),
-                ),
-                cull=configurations.Cull(
-                    2,
-                    fraction=0.1,
-                ),
-                tame=configurations.Tame(
-                    2,
-                    price=250,
-                ),
-            ),
-        ),
+        configuration=configuration,
     ),
 )
 
