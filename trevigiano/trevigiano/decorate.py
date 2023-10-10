ccc9a2ae609dd81d04499c7ed377e61ede3cafba
aurealisch <aurealisch>
2023-10-10 17:06:38 +0300

    🎀 The text decoration has been changed

--- trevigiano/trevigiano/decorate.py
+++ trevigiano/trevigiano/decorate.py
@@ -1,2 +1,8 @@
-def decorate(number: str) -> str:
-    return f'**{number}**'
+def bold(text: str) -> str:
+    return f'**{text}**'
+
+def highlighted(text: str) -> str:
+    return f'`{text}`'
+
+def decorate(text: str) -> str:
+    return bold(highlighted(text))
